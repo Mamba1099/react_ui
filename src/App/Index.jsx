393369ba2d722b410c4b7a630997414f46fc6d1b
Mamba1099 <mambakaranja240@gmail.com>
2024-03-10 06:46:17 +0300

    config: configurations for the react app

--- src/App/Index.jsx
+++
@@ -1,6 +0,0 @@
-import { withIsMobileViewProvider } from "./Context/provider";
-import App from "./App"; // Import the 'App' component
-
-const NamedApp = withIsMobileViewProvider(App); // Add a name to the export statement
-
-export default NamedApp;
