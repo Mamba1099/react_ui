56f2b5fa2dbc41d7e6150b3d2a0cbbedd94418d0
Mamba1099 <mambakaranja240@gmail.com>
2024-03-10 18:07:42 +0300

    import usetoggle hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import {useBoolean} from "@chakra-ui/react"
+import UseToggle from "./Hooks/UseToggle"
 
 export default function App() {
-  const [flag, setFlag] = useBoolean()
-
   return (
-    <>
-    <p>Boolean state: {flag.toString()}</p>
-    <button onClick={setFlag.toggle}>Click me to change state</button> {/*toggle is function to negate the boolean value*/}
-    </>
-  )
+    <div>
+      <UseToggle />
+    </div>
+  );
 
 }
 
